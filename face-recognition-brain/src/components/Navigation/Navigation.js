8686df4a7b84acdc5e22523523d921c601777484
bassamkdev <sami@DESKTOP-8V851Q1.localdomain>
2019-05-08 15:37:31 -0700

    updating components

---
+++ face-recognition-brain/src/components/Navigation/Navigation.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Navigation = () =>{
+	return (
+		<nav style = {{display: 'flex', justifyContent: 'flex-end'}}>
+			<p className = 'f3 link dim underline pa3 black pointer'>Sign Out</p>
+		</nav>
+	)
+}
+
+export default Navigation;
