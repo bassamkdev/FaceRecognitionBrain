d154c456c15c2b7d4fa21a6cabcc74e7dcb9c008
bassamkdev <sami@DESKTOP-8V851Q1.localdomain>
2019-05-08 16:18:14 -0700

    adding Logo component

--- face-recognition-brain/src/App.js
+++ face-recognition-brain/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
-       {//<Logo/>
-      //         <ImageLinkForm/>
-      //         <FaceRecognition/>
-       }  
+      <Logo/>
+      {/*<ImageLinkForm/>
+            <FaceRecognition/>*/}
      </div>
   );
 }
